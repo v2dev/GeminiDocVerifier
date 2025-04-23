21ce0a965cbf9bf511fdc96bc132f0afe622d2d1
Anilkumar-CCM <AnilKumar.G_EXT@myccmortgage.com>
2025-04-23 22:36:48 +0530

    Resolved the issues and also displayed the images snippet along with JSON in tabular format.

--- GeminiDocVerifier/Controllers/HomeController.cs
+++ GeminiDocVerifier/Controllers/HomeController.cs
@@ -1,9 +1,8 @@
 using System.Diagnostics;
 using System.Text;
-using System.Text.Json.Serialization;
 using GeminiDocVerifier.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing.Matching;
+using Microsoft.Extensions.Hosting.Internal;
 using Newtonsoft.Json;
 
 namespace GeminiDocVerifier.Controllers
@@ -11,10 +10,12 @@ namespace GeminiDocVerifier.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHostEnvironment _hostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger, IHostEnvironment hostEnvironment)
+        {            
             _logger = logger;
+            _hostEnvironment = hostEnvironment;
         }
 
         [HttpGet]
@@ -24,7 +25,17 @@ namespace GeminiDocVerifier.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(FileUpload model)
+        public async Task<IActionResult> Index(FileUpload model)
+        {
+            model.FilePath = await SplitFile(model);
+            model.FileName = model.FormFile.FileName;
+            model.ContentType = model.FormFile.ContentType;
+            model.Json = await GetGeminiResponse(model);
+            model.FormResponse = JsonConvert.DeserializeObject<dynamic>(model.Json);
+            return View(model);
+        }
+
+        public async Task<string> GetGeminiResponse(FileUpload model)
         {
             HttpClient _client = new HttpClient();
             _client.BaseAddress = new Uri("https://generativelanguage.googleapis.com/");
@@ -39,8 +50,12 @@ namespace GeminiDocVerifier.Controllers
                 }
                 var fileContent = Convert.ToBase64String(fileBytes);
 
-                var jsonFile = "{'generationConfig':{'response_mime_type':'application/json'},'contents':[{'parts':[{'text':'Get all the fields and values based on document type with page'},{'inline_data':{'mime_type':'application/pdf','data':'" + fileContent + "'}}]}]}";
+                //var prompt = "Detect and analyze multiple documents within the provided file, process their content, extract relevant data, identify the document type, and generate a structured hierarchical JSON for each. Combine all responses into an array. Response {self.payStubTemplate} format if document type is paystub or payslip. Response {self.w2DocTemplate} format if document type is W2. If document type is not paystub or W2, return {self.unknownDocTemplate}."
+
+                var prompt = "Detect and analyze multiple documents within the provided file, process their content, extract relevant data, identify the document type and page number, and generate a structured hierarchical JSON for each. Combine all responses into an array.";
+                var jsonFile = "{'generationConfig':{'response_mime_type':'application/json'},'contents':[{'parts':[{'text':'" + prompt + "'},{'inline_data':{'mime_type':'" + model.FormFile.ContentType + "','data':'" + fileContent + "'}}]}]}";
                 HttpContent content = new StringContent(jsonFile, Encoding.UTF8, "application/json");
+                //gemini-2.5-pro-exp-03-25
                 _response = _client.PostAsync("v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBEhuDK2hxLpvYoMKOwAj1t8fVMOqhCnYE", content).Result;
             }
 
@@ -50,12 +65,36 @@ namespace GeminiDocVerifier.Controllers
 
             var _text = _json?.candidates[0]?.content?.parts[0].text;
 
-            var _parsedPDFjson = JsonConvert.DeserializeObject<List<GeminiResponse>>(_text?.ToString());
-            model.FormResponse = _parsedPDFjson;
-
-            return View(model);
+            return _text?.ToString();
         }
 
+        public async Task<string> SplitFile(FileUpload model)
+        {
+            string _datetime = DateTime.Now.ToString("yyyyMMddHHmmss");            
+            if (model.FormFile.Length > 0)
+            {
+                var _path = @$"{_hostEnvironment.ContentRootPath}\wwwroot\images\{_datetime}";
+                Directory.CreateDirectory(_path);
+
+                string filePathWithFileName = $@"{_path}\{model.FormFile.FileName}";
+
+                using (Stream fileStream = new FileStream(filePathWithFileName, FileMode.Create))
+                {
+                    await model.FormFile.CopyToAsync(fileStream);
+                }
+
+                if (model.FormFile.ContentType.ToLower().Contains("pdf"))
+                {
+                    var pdf = new PdfDocument(filePathWithFileName);
+                    for (int _counter = 0; _counter < pdf.Pages.Count; ++_counter)
+                    {
+                        var pdf_page = pdf.CopyPage(_counter);
+                        pdf_page.SaveAs(@$"{_path}\page_{_counter+1}.pdf");
+                    }
+                }
+            }
+            return $"/images/{_datetime}";
+        }
 
         public IActionResult Privacy()
         {
--- GeminiDocVerifier/Models/FileUpload.cs
+++ GeminiDocVerifier/Models/FileUpload.cs
@@ -2,8 +2,16 @@
 {
     public class FileUpload
     {
-        public IFormFile FormFile{get;set;}
+        public IFormFile FormFile { get; set; }
 
-        public List<GeminiResponse> FormResponse{get;set;}
-}
+        public string FilePath { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string FileName { get; set; }
+
+        public dynamic FormResponse { get; set; }
+
+        public string Json { get; set; }
+    }
 }
